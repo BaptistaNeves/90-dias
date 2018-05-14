e820d5e6f700616fee7969c962fef2b899217b4a
nik <nailovernail@gmail.com>
2018-05-13 23:25:40 -0300

    primeiro projeto

---
+++ form.php
@@ -0,0 +1,15 @@
+<?php $name = $_POST['nome'];
+$email   = $_POST['email'];
+$telefone = $_POST['telefone'];
+$celular = $_POST['celular'];
+$escolha = $_POST['escolha'];
+$mensagem =$_POST['mensagem'];
+$formcontent="De: $name \n\n Mensagem: $mensagem \n\n Telefone: $telefone \n Celular: $celular \n Celular é Whatsapp? $escolha";
+$recipient= "ngaribaldif@gmail.com";
+$subject= "MENSAGEM DO FORMULÁRIO";
+$mailheader="From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Erro!");
+echo "Muito obrigado, a sua mensagem foi enviada";
+?>
+
+
